cc754b6da5adcaa69fb809995a20cf2b26b05073
blumdani <blumdani@isu.edu>
2025-04-20 14:50:26 -0600

    More crowd fill

---
+++ Assets/Scripts/Menus/LostMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LostMenu : MonoBehaviour
+{
+    // Update is called once per frame
+    public void OnButtonClick()
+    {
+        if(this.gameObject.name == "Retry")
+        {
+            //Reset the bull health to 140
+            BullHealthController.health = 140;
+            SceneManager.LoadScene("Intro_Animation");
+        }
+        else if(this.gameObject.name == "ExitGame")
+        {
+            Application.Quit();
+        }
+    }
+}
